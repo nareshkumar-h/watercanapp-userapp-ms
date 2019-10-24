c0bc907c935a7dba4682978897b462e216caaba8
Anitha <anithanambi2000@gmail.com>
2019-10-24 11:37:17 +0530

    Added project setup for watercanappusermicroservice

---
+++ src/test/java/com/revature/watercanappuserms/WatercanappUserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.watercanappuserms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatercanappUserMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
